679d3e8edda1233ec5fdb6b592bdbd903de7bee7
Lars-ingel <Lars-ingel@users.noreply.github.com>
2020-03-20 15:26:30 +0100

    Feature 4 - Form front end

--- feature4.php
+++ feature4.php
@@ -15,6 +15,36 @@ include_once(__DIR__ . "/classes/User.php");
 </head>
 
 <body>
+    <h1> Gelieve eerst uw profiel te vervolledigen. </h1>
+    <div class="form__field">
+        <label for="Games">Games</label>
+        <input type="text" id="Games" name="games" placeholder="Vul u favoriete game in">
+    </div>
+
+    <div class="form__field">
+        <label for="Films">Films</label>
+        <input type="text" id="Films" name="films" placeholder="Vul u favoriete film in">
+    </div>
+
+
+    <div class="form__field">
+        <label for="Muziek">Muziek</label>
+        <input type="text" id="Muziek" name="muziek" placeholder="Vul u favoriete album in">
+    </div>
+
+
+    <div class="form__field">
+        <label for="Locatie">Locatie</label>
+        <input type="text" id="Locatie" name="locatie" placeholder="Vul u stad/dorp in">
+    </div>
+
+
+    <div class="form__field">
+        <label for="Boeken">Boeken</label>
+        <input type="text" id="Boeken" name="boeken" placeholder="Vul u favoriete boek in">
+    </div>
+
+    <a href="index.php"> <button>Bevestigen</button> </a>
 
 </body>
 
