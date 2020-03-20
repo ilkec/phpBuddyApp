5d3d59ff7a7dbf7ca692387dacdc1cccd3eef9d7
Lars-ingel <Lars-ingel@users.noreply.github.com>
2020-03-20 15:57:38 +0100

    Settings

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+const SETTINGS = [
+    "db" => [
+        "user" => "root",
+        "passwoord" => "",
+        "host" => "localhost",
+        "port" => 3308,
+        "dbname" => "buddyapp"
+    ]
+];
