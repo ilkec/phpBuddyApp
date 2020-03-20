e49186132f407e1752e2ffa1bc0b90099dba2f15
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-03-20 13:36:56 +0100

    added register page with register form

---
+++ register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Registreren</title>
+</head>
+<body>
+    
+    <div>
+        <form action="" method="post">
+            <h2>Registreren</h2>
+            <div>
+                <label for="firstname">Voornaam</label>
+                <input type="text" placeholder="Voornaam" name="firstname" id="firstname">
+            </div>
+            <div>
+                <label for="lastname">Achternaam</label>
+                <input type="text" placeholder="Achternaam" name="lastname" id="lastname">
+            </div>
+            <div>
+                <label for="email">Emailadres</label>
+                <input type="text" placeholder="Emailadres" name="email" id="email">
+            </div>
+            <div>
+                <label for="password">Wachtwoord</label>
+                <input type="password" placeholder="Wachtwoord" name="password" id="password">
+            </div>
+            <div>
+                <input type="submit" value="Registreer" class="btn btn-success">
+            </div>
+
+
+
+
+        </form>
+    </div>
+
+</body>
+</html>
