9f816f7340e9005eadd259d2a747521c629c1685
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-03-21 18:50:06 +0100

    functions getAll and update working

--- profile.php
+++ profile.php
@@ -1,6 +1,41 @@
 <?php
 
 include_once(__DIR__ .'/classes/User.php');
+include_once(__DIR__ . '/classes/Db.php');
+
+
+$user = new User();
+$user->setId(1);
+$getAllUser = $user->getAll();
+
+
+if(!empty($_POST)){
+    /*if(empty($_POST['password'])){*/
+        try {
+            
+            $user->setEmail($_POST['email']);
+            $user->setFirstname($_POST['firstname']);
+            $user->setLastname($_POST['lastname']);
+            echo $user->getEmail();
+            
+            $user->update();
+
+            
+        } 
+        catch (\Throwable $th) {
+                $error = $th->getMessage();
+        }
+    }
+    
+    
+
+
+//Gegevens van User ophalen 
+//gegevens van user plaatsen in inputvelden behalve wachtwoord
+// profilePicutre ophalen uit device van persoon
+//profilePicture kunnen opslaan in database = url sturen
+
+//
 
 
 ?>
@@ -20,6 +55,11 @@ include_once(__DIR__ .'/classes/User.php');
         .form_field{
             margin:10px;
         }
+        .profilePicture{
+            width: 100px;
+            height: 100px;
+            background-color: grey;
+        }
 
     </style>
 </head>
@@ -28,25 +68,33 @@ include_once(__DIR__ .'/classes/User.php');
     <div class="form">
         <form action="" method="post">
             <h2>Persoonlijke gegevens</h2>
+            <div>
+                <div class="profilePicture">
+                </div>
+                <input type="file" class="btn" id="btnBrowse">
+
+            </div>
+            <?php /*foreach ($getAllUser as $user) :*/ ?>
             <div class="form_field">
                 <label for="firstname">Voornaam</label>
-                <input type="text" placeholder="Voornaam" name="firstname" id="firstname">
+                <input type="text" value=" <?php  echo $getAllUser[0]['firstname'];?> " name="firstname" id="firstname">
             </div>
             <div class="form_field">
                 <label for="lastname">Achternaam</label>
-                <input type="text" placeholder="Achternaam" name="lastname" id="lastname">
+                <input type="text"value=" <?php  echo $getAllUser[0]['lastname'];?> " name="lastname" id="lastname">
             </div>
             <div class="form_field">
                 <label for="email">Emailadres</label>
-                <input type="text" placeholder="Emailadres" name="email" id="email">
+                <input type="text" value=" <?php  echo $getAllUser[0]['email'];?> " name="email" id="email">
             </div>
-            <div class="form_field">
+            <!--<div class="form_field">
                 <label for="password">Wachtwoord</label>
-                <input type="password" placeholder="Wachtwoord" name="password" id="password">
-            </div>
+                <input type="password" placeholder="nieuw wachtwoord" name="password" id="password">
+            </div>--->
+    <?php /*endforeach; */?>
             <div class="form_field">
-                <input type="submit" value="Opslaan" class="btn btn-success">
-                <input type="submit" value="Cancel" class="btn btn-secondary">
+                <input type="submit" value="Opslaan" class="btn btn-success" id="btnOpslaan"> 
+                <input type="button" value="Cancel" class="btn btn-secondary" id="btnCancel">
             </div>
         </form>
     </div>
