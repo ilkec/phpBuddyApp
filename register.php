7abd1d6764072cbf9603256d988084a842056bc2
Esat <r0765162@student.thomasmore.be>
2020-03-25 18:49:38 +0100

    HTML: css relinked and html cleaned.

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style_register.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Registreren</title>
   </head>
 
@@ -13,7 +13,7 @@
     <!-----------------------------Navbar------------------------------>
     <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <div class="container-fluid"> <a class="navbar-brand" href="#">MyBuddyApp</a>
-        <ul class="nav justify-content-end"> <a class="nav-link disabled" href="#">Login</a> </ul>
+        <ul class="nav justify-content-end"> <a class="nav-link" href="#">Login</a> </ul>
       </div>
     </nav>
     
@@ -68,7 +68,6 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="Js/Javascript.js"></script>
   </body>
 
 </html>
