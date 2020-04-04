c48d5b9fc784b3732136b0214bf53644e64e0fc3
Lars-ingel <Lars-ingel@users.noreply.github.com>
2020-04-04 12:40:10 +0200

    styling log in

--- feature9.php
+++ feature9.php
@@ -1,68 +1,76 @@
 <?php
 
-    include_once(__DIR__ .'/classes/User.php');
+include_once(__DIR__ . '/classes/User.php');
 
-    $user = new User();
-    session_start();
-    $databaseId = $user->getDatabaseId();
-    $user->setId($databaseId['id']);
+$user = new User();
+session_start();
+$databaseId = $user->getDatabaseId();
+$user->setId($databaseId['id']);
 
-    if(isset($_SESSION['user'])) {
-        $matches = $user->showMatches();
-    }else{
-        header("Location: feature2.php");
-    }
-    //var_dump($matches);
+if (isset($_SESSION['user'])) {
+    $matches = $user->showMatches();
+} else {
+    header("Location: feature2.php");
+}
+//var_dump($matches);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Matches</title>
     <style>
-        .card{
+        .card {
             width: 500px;
             margin-bottom: 20px;
         }
-        .wrapper{
+
+        .wrapper {
             margin: 20px 0 0 20px;
         }
-        .profilePicture{
+
+        .profilePicture {
             width: 50px;
-            margin-left:auto;
-            margin-right:auto;
+            margin-left: auto;
+            margin-right: auto;
 
         }
-        .card-body{
+
+        .card-body {
             display: grid;
-            grid-template-columns: 1fr 3fr 1fr;  
+            grid-template-columns: 1fr 3fr 1fr;
         }
-        
-        p{
+
+        p {
             text-align: center;
         }
     </style>
 </head>
+
 <body>
-<div class="wrapper"> 
-<h3>Match made between</h3>
-<?php foreach($matches as $match) :?>
-    <div class="card">
-        <div class="card-body">
-            <img src="<?php if($match['picture1'] === NULL){
-                    echo "uploads/profilePic.png";
-                    } else{
-                        echo "uploads/" . $match['picture1'];} ?>" alt="profiel foto" class="profilePicture">
-             <p><?php echo  $match['firstname1'] . " " . $match['lastname1'] . " &amp; " . $match['firstname2'] . " " . $match['lastname2'] ?>  </p>
-            <img src="<?php if($match['picture2'] === NULL){
-                    echo "uploads/profilePic.png";
-                    } else{
-                        echo "uploads/" . $match['picture2'];} ?>" alt="profiel foto" class="profilePicture">
-        </div>
+    <div class="wrapper">
+        <h3>Match made between</h3>
+        <?php foreach ($matches as $match) : ?>
+            <div class="card">
+                <div class="card-body">
+                    <img src="<?php if ($match['picture1'] === NULL) {
+                                    echo "uploads/profilePic.png";
+                                } else {
+                                    echo "uploads/" . $match['picture1'];
+                                } ?>" alt="profiel foto" class="profilePicture">
+                    <p><?php echo  $match['firstname1'] . " " . $match['lastname1'] . " &amp; " . $match['firstname2'] . " " . $match['lastname2'] ?> </p>
+                    <img src="<?php if ($match['picture2'] === NULL) {
+                                    echo "uploads/profilePic.png";
+                                } else {
+                                    echo "uploads/" . $match['picture2'];
+                                } ?>" alt="profiel foto" class="profilePicture">
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-    <?php  endforeach; ?> 
-</div>      
 </body>
+
 </html>
