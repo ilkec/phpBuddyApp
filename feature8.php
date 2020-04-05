8b26b40a4bbd15e9524170461a2818f9209b59ef
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-04-05 10:08:49 +0200

    trying to make a basic chat box

--- feature8.php
+++ feature8.php
@@ -1,5 +1,51 @@
 <?php 
 
+include_once(__DIR__ .'/classes/User.php');
+$user = new User();
+session_start();
+$user->setEmail($_SESSION['user']);
+$databaseId = $user->getDatabaseId();
+$user->setId($databaseId['id']);
+//var_dump($databaseId['id']);
+$getAllUser = $user->getAll();
+
+$firstname = $getAllUser['firstname'];
+var_dump($firstname);
+var_dump($databaseId);
+
+/*$user2 = new User();
+$user2->setId(14);
+$getAllUser2 = $user2->getAll();*/
+
+$idReceiver = 14;
+
+//// 1. kijken wie de ingelogde gebruiker is, dit wordt de zender van het bericht (getAllUser['firstname']);
+//// 2. kijken wie de persoon in de buddysuggestie was, dit wordt de ontvanger van het bericht. (om te testen hardcoded in code zetten)
+//// 3. als er gesubmit wordt bericht van zender ophalen uit textarea (input) en setten in setMessage();
+//// 4. als er gesubmit wordt bericht sturen naar databank met id van zender en id van ontvanger. 
+//// 5. berichten uit de databank halen waar het id van de user bij staat. deze berichten tonen in de 'chatbox'
+
+
+if(!empty($_POST)){
+    $message = $_POST['message']; //message moet later naar databank gestuurd worden met id van sender en id van receiver
+    $user->setMessage($message);
+    $user->setToUser($idReceiver);
+    $user->setFromUser($databaseId['id']);
+    var_dump($message);
+    $user->sendMessage();
+
+    
+
+}
+
+    $user->setToUser($idReceiver);
+    $user->setFromUser($databaseId['id']);
+    $koekoek = $user->messagesFromDatabase();
+    var_dump($koekoek);
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +54,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .chatbox{
+            height: 250px;
+            width: 250px;
+            background-color:aqua;
+        }
+    </style>
 </head>
 <body>
-    
+    <div class="chatbox">
+        <?php foreach($koekoek as $piep) :?>
+        <p><?php echo $piep['fromUser'] . " " . $piep['message'] ?></p>
+        <?php endforeach; ?>
+    </div>
+    <form class="container w-25 border border-primary rounded" action="" method="post" enctype="multipart/form-data">
+
+        <div class="mt-2">
+            <label for="message">Message</label>
+
+            <textarea class="form-control" type="text" placeholder="message" name="message" id="message"></textarea>
+        </div>
+        <div>
+            <input type="submit" value="send" class="btn btn-primary mb-3" id="btnOpslaan" name="sendMessage">
+        </div>
+    </form>
 </body>
 </html>
