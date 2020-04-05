46ce77525017a5ff48eceaecd9302f170fb712e2
Esat <r0765162@student.thomasmore.be>
2020-04-05 20:58:47 +0200

    JS & AJAX: js folder created and users have been successfully sorted by interest without refreshing the page with ajax.

---
+++ js/script.js
@@ -0,0 +1,107 @@
+$(document).ready(function () {
+  $("a").click(function (e) {
+    e.preventDefault();
+    let idBtn = $(this).attr('id');
+    let list = $(".usersList");
+    let errormsg = '<div class="alert alert-light" role="alert" style="font-size:14px">No user with the same interest was found.😕</div>';
+    let errorForLocation = '<div class="alert alert-light" role="alert" style="font-size:14px">No user with the same location was found.😕</div>';
+    let errorAlert = $('.usersList');
+    console.log(idBtn);
+    if (idBtn === 'book') {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/sortByBook.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          
+         if(respons.trim()==''){
+             errorAlert.html(errormsg);
+          }
+            
+        }
+      });
+    }else if (idBtn === 'music') {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/sortByMusic.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          
+         if(respons.trim()==''){
+             errorAlert.html(errormsg);
+          }
+            
+        }
+      });
+    }
+    else if (idBtn === 'game') {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/sortByGame.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          console.log(respons);
+          if(respons.trim()==''){
+             errorAlert.html(errormsg);
+          }
+        }
+      });
+    }
+    else if (idBtn === 'movie') {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/sortByMovie.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          if(respons.trim()==''){
+             errorAlert.html(errormsg);
+          }
+        }
+      });
+    }
+    else if (idBtn === 'location') {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/sortByLocation.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          console.log(respons);
+          if(respons.trim()==''){
+             errorAlert.html(errorForLocation);
+          }
+        }
+      });
+    }
+    else {
+      e.preventDefault();
+      $.ajax({
+        method: "post"
+        , url: "./userFiltering/showAllUsers.php"
+        , data: $('#displayData').serialize()
+        , dataType: "html"
+        , success: (respons) => {
+          $('.usersList').html(respons);
+          console.log(respons);
+          if(respons.trim()==''){
+             errorAlert.html(errormsg);
+          }
+        }
+      });
+    }
+  });
+});
