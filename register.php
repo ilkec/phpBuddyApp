557d2f72a7217558f4c944c76a4954881be827b8
Lars-ingel <Lars-ingel@users.noreply.github.com>
2020-04-06 15:56:40 +0200

    added logo to navbar

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
-include_once(__DIR__."/classes/User.php");
-if(!empty($_POST)){
+include_once(__DIR__ . "/classes/User.php");
+if (!empty($_POST)) {
   try {
     $user = new User();
     $user->setFirstname($_POST['firstname']);
@@ -12,10 +12,8 @@ if(!empty($_POST)){
     $user->setConfPassword($_POST['confPassword']);
     $user->saveUser();
     $success = "Your account has been created successfully";
-    
-  }catch(\Throwable $th) {
-    $error=$th->getMessage();
-   
+  } catch (\Throwable $th) {
+    $error = $th->getMessage();
   }
 }
 
@@ -23,39 +21,39 @@ if(!empty($_POST)){
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Registreren</title>
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Registreren</title>
+</head>
 
-  <body>
-    <!-----------------------------Navbar------------------------------>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <div class="container-fluid"> <a class="navbar-brand" href="#">MyBuddyApp</a>
-        <ul class="nav justify-content-end"> <a class="nav-link" href="feature2.php">Login</a> </ul>
-      </div>
-    </nav>
-    <!------------------------Error-message------------------------->
-    <?php if(isset($error)):?>
+<body>
+  <!-----------------------------Navbar------------------------------>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container-fluid"> <a class="navbar-brand" href="#"><img src="img/Logo.png" width="70em" alt="MyBuddyApp"></a>
+      <ul class="nav justify-content-end"> <a class="nav-link" href="feature2.php">Login</a> </ul>
+    </div>
+  </nav>
+  <!------------------------Error-message------------------------->
+  <?php if (isset($error)) : ?>
     <div class="alert alert-danger alert-dismissible fade show" id="alert" role="alert">
       <?php echo $error ?>
     </div>
-    <?php endif;?>
-    <!-----------------------End-Error-message----------------------->
-    <!------------------------success-message------------------------->
-    <?php if(isset($success)):?>
+  <?php endif; ?>
+  <!-----------------------End-Error-message----------------------->
+  <!------------------------success-message------------------------->
+  <?php if (isset($success)) : ?>
     <div class="alert alert-success alert-dismissible fade show" role="alert">
       <?php echo $success ?>
     </div>
-    <?php endif;?>
-    <!-----------------------End-sucess-message------------------------->
-    <!-----------------------------Form------------------------------>
-    <div class="form-div">
-      <!----------------------------reset-form-after-user-created successfully-------------------------->
-      <?php if(isset($success)):?>
+  <?php endif; ?>
+  <!-----------------------End-sucess-message------------------------->
+  <!-----------------------------Form------------------------------>
+  <div class="form-div">
+    <!----------------------------reset-form-after-user-created successfully-------------------------->
+    <?php if (isset($success)) : ?>
       <form action="register.php" method="post">
         <h1 class="title">Sign up</h1>
         <!--------------Email------------>
@@ -93,22 +91,28 @@ if(!empty($_POST)){
         <!--------------Button------------>
         <button type="submit" class="btn btn-primary button">Sign Up</button>
       </form>
-      <?php else: ?>
+    <?php else : ?>
       <!----------------------------if-there-is-an-error-form-contains-the-user-info------------------------->
       <form action="register.php" method="post">
         <h1 class="title">Sign up</h1>
         <!--------------Email------------>
         <div class="form-group email">
           <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control formInputs" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Example@student.thomasmore.be" value="<?php if(isset($_POST['email'])){echo $user->getEmail();} ?>"> </div>
+          <input type="email" class="form-control formInputs" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Example@student.thomasmore.be" value="<?php if (isset($_POST['email'])) {
+                                                                                                                                                                                      echo $user->getEmail();
+                                                                                                                                                                                    } ?>"> </div>
         <!--------------Name------------>
         <div class="row" id="name">
           <div class="col">
             <label for="fullName">First name</label>
-            <input type="text" class="form-control formInputs" name="firstname" placeholder="first name" value="<?php if(isset($_POST['firstname'])){echo $user->getFirstname();}?>"> </div>
+            <input type="text" class="form-control formInputs" name="firstname" placeholder="first name" value="<?php if (isset($_POST['firstname'])) {
+                                                                                                                  echo $user->getFirstname();
+                                                                                                                } ?>"> </div>
           <div class="col">
             <label for="fullName">Last name</label>
-            <input id="lasnemText" type="text" class="form-control formInputs" placeholder="Last name" name="lastname" value="<?php if(isset($_POST['lastname'])){echo $user->getLastname();}?>"> </div>
+            <input id="lasnemText" type="text" class="form-control formInputs" placeholder="Last name" name="lastname" value="<?php if (isset($_POST['lastname'])) {
+                                                                                                                                echo $user->getLastname();
+                                                                                                                              } ?>"> </div>
         </div>
         <!-------------Gender----------!-->
         <div class="form-row gender">
@@ -120,7 +124,9 @@ if(!empty($_POST)){
         <!-------------Birthday----------!-->
         <div class="form-group email ">
           <label for="fullName">Birthday</label>
-          <input type="text" class="form-control formInputs" placeholder="dd/mm/yy" name="birthday" value="<?php if(isset($_POST['birthday'])){echo $user->getBirthday();}?>"> </div>
+          <input type="text" class="form-control formInputs" placeholder="dd/mm/yy" name="birthday" value="<?php if (isset($_POST['birthday'])) {
+                                                                                                              echo $user->getBirthday();
+                                                                                                            } ?>"> </div>
         <!-------------Password----------!-->
         <div class="form-group password ">
           <label for="exampleInputPassword1">Password</label>
@@ -132,11 +138,11 @@ if(!empty($_POST)){
         <!--------------Button------------>
         <button type="submit" class="btn btn-primary button">Sign Up</button>
       </form>
-      <?php endif; ?>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
+    <?php endif; ?>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
 
 </html>
--- sortBuddy.php
+++ sortBuddy.php
@@ -1,19 +1,19 @@
-<?php 
-include_once(__DIR__ .'/classes/User.php');
+<?php
+include_once(__DIR__ . '/classes/User.php');
 include_once(__DIR__ . '/classes/Db.php');
 
 $user = new User();
 session_start();
 
 $user->setEmail($_SESSION['user']);
-$connectedUserEmail= $_SESSION['user'];
+$connectedUserEmail = $_SESSION['user'];
 
 /*------------------------------get-the-currently-logged-in-user-interest----------------------------------------------*/
 $connectedUserFirstname = $user->getConnectedUserFirstname();
 $user->setFirstname($connectedUserFirstname['firstname']);
 $connectedUserlastname = $user->getConnectedUserLastname();
 $user->setLastname($connectedUserlastname['lastname']);
-$userFullname =$connectedUserFirstname['firstname']." ". $connectedUserlastname['lastname'];
+$userFullname = $connectedUserFirstname['firstname'] . " " . $connectedUserlastname['lastname'];
 $connectedUserPicture = $user->getConnectedUserPicture();
 $user->setProfilePicture($connectedUserPicture['picture']);
 $connectedUserGame = $user->getConnectedUserGame();
@@ -30,12 +30,11 @@ $user->setMusic($connectedUserMusic['music']);
 
 
 
-if( isset($_SESSION['user']) ) {
+if (isset($_SESSION['user'])) {
   $conn = Db::getConnection();
 
   //$query = "select firstname from users where games = '$curentUserGame' and email != '$curntUserEmail'";
   $query = "select firstname, lastname,picture from users where email != '$connectedUserEmail'";
-  
 }
 
 ?>
@@ -44,63 +43,68 @@ if( isset($_SESSION['user']) ) {
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-  </head>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
 
-  <body>
-    <!-----------------------------Navbar------------------------------>
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <div class="container-fluid"> <a class="navbar-brand" href="#">MyBuddyApp</a>
-        <ul class="nav justify-content-end"> <a class="nav-link" href=""><img src="<?php if($connectedUserPicture['picture'] === NULL){ echo "uploads/profilePic.png";}else{
-  echo "uploads/".$connectedUserPicture['picture'];} ?>" class="avatar"><?php echo $connectedUserFirstname['firstname']." ".$connectedUserlastname['lastname']?></a></ul>
-      </div>
-    </nav>
-    <div class="container-fluid box ">
-      <form class="form-inline userForm"  method="post" >   
-        <a href="" type="submit" name="bookBtn" id="book">
-          <div class="type-select btn btn-primary bookBtn"><i class="fas fa-book-open icon"></i>Book:  <span class="badge badge-dark"><?php echo $connectedUserBook['books']?></span></div>
-        </a>
-        <a href="" id="movie" type="submit" name="movieBtn" data-sortBy="movie">
-          <div class="type-select btn btn-primary movieBtn" data-value="user"><i class="fas fa-film icon"></i>Movie: <span class="badge badge-dark"><?php echo $connectedUserMovie['films']?></span> </div>
-        </a>
-        <a href="" type="submit" name="musicBtn" id="music">
-          <div class="type-select btn btn-primary musicBtn"><i class="fas fa-music icon"></i>Music: <span class="badge badge-dark"><?php echo $connectedUserMusic['music']?></span></div>
-        </a>
-        <a href="" id="game" type="submit" name="gameBtn">
-          <div class="type-select btn btn-primary gameBtn" data-value="3"><i class="fas fa-gamepad icon"></i>Game: <span class="badge badge-dark"><?php echo $connectedUserGame['games']?></span></div>
-        </a>
-        <a href="" id="location" type="submit" name="locationBtn" data-sortBy="location">
-          <div class="type-select btn btn-primary" data-value="organization"><i class="fas fa-map-marker-alt icon"></i>Location: <span class="badge badge-dark"><?php echo $connectedUserLocation['location']?></span></div>
-        </a>
-        <a href="" id="all" type="submit" name="showAllBtn" data-sortBy="showAllBtn">
-          <div class="type-select btn btn-primary"><i class="fas fa-list icon"></i>Show all</div>
-        </a>
-      </form>
-      <div class="container-list">
-        <div class="userContainer">
-          <ul class="usersList">
-             <?php foreach($conn->query($query) as $data): ?>
-            <li class="row"> <img src="<?php if($data['picture'] === NULL){
-  echo "uploads/profilePic.png ";
-} else{
-  echo "uploads/".$data['picture'];} ?>" class="avatar">
-              <h2 class="user-name col-xs-5"><?php echo $data['firstname']." ".$data['lastname']?></h2> </li>
-             <?php endforeach; ?>
-          </ul>
-        </div>
+<body>
+  <!-----------------------------Navbar------------------------------>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container-fluid"> <a class="navbar-brand" href="#"><img src="img/Logo.png" width="70em" alt="MyBuddyApp"></a>
+      <ul class="nav justify-content-end"> <a class="nav-link" href=""><img src="<?php if ($connectedUserPicture['picture'] === NULL) {
+                                                                                    echo "uploads/profilePic.png";
+                                                                                  } else {
+                                                                                    echo "uploads/" . $connectedUserPicture['picture'];
+                                                                                  } ?>" class="avatar"><?php echo $connectedUserFirstname['firstname'] . " " . $connectedUserlastname['lastname'] ?></a></ul>
+    </div>
+  </nav>
+  <div class="container-fluid box ">
+    <form class="form-inline userForm" method="post">
+      <a href="" type="submit" name="bookBtn" id="book">
+        <div class="type-select btn btn-primary bookBtn"><i class="fas fa-book-open icon"></i>Book: <span class="badge badge-dark"><?php echo $connectedUserBook['books'] ?></span></div>
+      </a>
+      <a href="" id="movie" type="submit" name="movieBtn" data-sortBy="movie">
+        <div class="type-select btn btn-primary movieBtn" data-value="user"><i class="fas fa-film icon"></i>Movie: <span class="badge badge-dark"><?php echo $connectedUserMovie['films'] ?></span> </div>
+      </a>
+      <a href="" type="submit" name="musicBtn" id="music">
+        <div class="type-select btn btn-primary musicBtn"><i class="fas fa-music icon"></i>Music: <span class="badge badge-dark"><?php echo $connectedUserMusic['music'] ?></span></div>
+      </a>
+      <a href="" id="game" type="submit" name="gameBtn">
+        <div class="type-select btn btn-primary gameBtn" data-value="3"><i class="fas fa-gamepad icon"></i>Game: <span class="badge badge-dark"><?php echo $connectedUserGame['games'] ?></span></div>
+      </a>
+      <a href="" id="location" type="submit" name="locationBtn" data-sortBy="location">
+        <div class="type-select btn btn-primary" data-value="organization"><i class="fas fa-map-marker-alt icon"></i>Location: <span class="badge badge-dark"><?php echo $connectedUserLocation['location'] ?></span></div>
+      </a>
+      <a href="" id="all" type="submit" name="showAllBtn" data-sortBy="showAllBtn">
+        <div class="type-select btn btn-primary"><i class="fas fa-list icon"></i>Show all</div>
+      </a>
+    </form>
+    <div class="container-list">
+      <div class="userContainer">
+        <ul class="usersList">
+          <?php foreach ($conn->query($query) as $data) : ?>
+            <li class="row"> <img src="<?php if ($data['picture'] === NULL) {
+                                          echo "uploads/profilePic.png ";
+                                        } else {
+                                          echo "uploads/" . $data['picture'];
+                                        } ?>" class="avatar">
+              <h2 class="user-name col-xs-5"><?php echo $data['firstname'] . " " . $data['lastname'] ?></h2>
+            </li>
+          <?php endforeach; ?>
+        </ul>
       </div>
     </div>
-    <div id="#displayData"></div>
-    <script src="https://kit.fontawesome.com/6792ce1460.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="js/script.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  </body>
+  </div>
+  <div id="#displayData"></div>
+  <script src="https://kit.fontawesome.com/6792ce1460.js" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/script.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</body>
 
 </html>
