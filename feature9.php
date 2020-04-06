49641ad0962af467789c103258cb6569cad0aede
Bo Dockx <bo.dockx@gmail.com>
2020-04-06 16:59:38 +0200

    ftr9

---
+++ feature9.php
@@ -0,0 +1,76 @@
+<?php
+
+include_once(__DIR__ . '/classes/User.php');
+
+$user = new User();
+session_start();
+$databaseId = $user->getDatabaseId();
+$user->setId($databaseId['id']);
+
+if (isset($_SESSION['user'])) {
+    $matches = $user->showMatches();
+} else {
+    header("Location: feature2.php");
+}
+//var_dump($matches);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Matches</title>
+    <style>
+        .card {
+            width: 500px;
+            margin-bottom: 20px;
+        }
+
+        .wrapper {
+            margin: 20px 0 0 20px;
+        }
+
+        .profilePicture {
+            width: 50px;
+            margin-left: auto;
+            margin-right: auto;
+
+        }
+
+        .card-body {
+            display: grid;
+            grid-template-columns: 1fr 3fr 1fr;
+        }
+
+        p {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <h3>Match made between</h3>
+        <?php foreach ($matches as $match) : ?>
+            <div class="card">
+                <div class="card-body">
+                    <img src="<?php if ($match['picture1'] === NULL) {
+                                    echo "uploads/profilePic.png";
+                                } else {
+                                    echo "uploads/" . $match['picture1'];
+                                } ?>" alt="profiel foto" class="profilePicture">
+                    <p><?php echo  $match['firstname1'] . " " . $match['lastname1'] . " &amp; " . $match['firstname2'] . " " . $match['lastname2'] ?> </p>
+                    <img src="<?php if ($match['picture2'] === NULL) {
+                                    echo "uploads/profilePic.png";
+                                } else {
+                                    echo "uploads/" . $match['picture2'];
+                                } ?>" alt="profiel foto" class="profilePicture">
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
