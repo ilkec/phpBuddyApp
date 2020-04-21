191126d0aa3b9c90c84016897e75f21d105a2ca4
Lars-ingel <Lars-ingel@users.noreply.github.com>
2020-04-21 16:30:26 +0200

    WORKING brute force protection, form blocks no auto refresh

--- feature2.php
+++ feature2.php
@@ -3,7 +3,29 @@
 include_once(__DIR__ . '/classes/User.php');
 include_once(__DIR__ . '/classes/Db.php');
 
+/*
+session_start();
 
+if ($user->canLogin() != true) {
+	$_SESSION["login_attempts"] += 1;
+}
+// At the top of page right after session_start();
+if (isset($_SESSION["locked"])) {
+	$difference = time() - $_SESSION["locked"];
+	if ($difference > 30) {
+		unset($_SESSION["locked"]);
+		unset($_SESSION["login_attempts"]);
+	}
+}
+
+// In sign-in form submit button
+if ($_SESSION["login_attempts"] > 2) {
+	$_SESSION["locked"] = time();
+	echo "Please wait for 30 seconds";
+} */
+
+$block = "";
+$disable = "";
 if (!empty($_POST)) {
 
 	$user = new User();
@@ -11,9 +33,20 @@ if (!empty($_POST)) {
 	$password = $_POST['password'];
 
 
-
 	if (!empty($email) && !empty($password)) {
-
+		if ($user->canLogin($email, $password) != true) {
+			if (isset($_COOKIE['login-form'])) {
+				if ($_COOKIE['login-form'] < 3) {
+					$attempts = $_COOKIE['login-form'] + 1;
+					setcookie('login-form', $attempts, time() + 60); // cookie 60 seconden met aantal pogingen
+				} else {
+					$block = 'Te veel foute invoerpogingen, gelieve 1minuut te wachten.';
+					$disable = 'disabled';
+				}
+			} else {
+				setcookie('login-form', 1, time() + 60); // cookie 60 seconden met waarde 1
+			}
+		}
 		if ($user->canLogin($email, $password)) {
 			//$user->setEmail($email);
 			//$user->setPassword($password);
@@ -64,23 +97,26 @@ if (!empty($_POST)) {
 </head>
 
 <body class="container-fluid bg-secondary">
-	<div class="row">
-		<img src="img/we-are-imd.jpg" alt="weareimd" class="col-lg-6 col-md-6 d-none d-md-block" id="login-img">
-		<form class="col-lg-6 col-md-6" id="login-form" action="" method="post">
-			<h2 form__title>Sign up for an account</h2>
-			<?php if (isset($error)) : ?>
-				<p>
-					<?php echo $error; ?>
-				</p>
-			<?php endif; ?>
-			<label for="email">Email</label>
-			<input class="form-control" type="text" id="email" name="email">
-			<label for="password">Password</label>
-			<input class="form-control" type="password" id="password" name="password">
-			<input class="mt-3 mb-3 btn btn-primary" type="submit" value="Log in">
-			<a href="register.php" class="mt-3 mb-3 btn btn-primary" type="submit" value="Log in">Register</a>
-		
-	</div>
+	<fieldset <?php echo $disable ?>>
+		<div class="row">
+			<img src="img/we-are-imd.jpg" alt="weareimd" class="col-lg-6 col-md-6 d-none d-md-block" id="login-img">
+			<form class="col-lg-6 col-md-6" id="login-form" action="" method="post">
+				<h2 form__title>Sign up for an account</h2>
+				<?php if (isset($error)) : ?>
+					<p>
+						<?php echo $error; ?>
+					</p>
+				<?php endif; ?>
+				<label for="email">Email</label>
+				<input class="form-control" type="text" id="email" name="email">
+				<label for="password">Password</label>
+				<input class="form-control" type="password" id="password" name="password">
+				<h5 class="blockform"><?php echo $block ?></h5>
+				<input class="mt-3 mb-3 btn btn-primary" type="submit" $disable value="Log in">
+				<a href="register.php" class="mt-3 mb-3 btn btn-primary" type="submit" value="Log in">Register</a>
+
+		</div>
+	</fieldset>
 </body>
 
 </html>
