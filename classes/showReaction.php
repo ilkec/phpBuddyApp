67f48c01543e1ec7f73a15c813015ecb55c07d2e
Esat <r0765162@student.thomasmore.be>
2020-05-02 17:24:07 +0200

    SQL: sql replaced with PDO prepare.

---
+++ classes/showReaction.php
@@ -0,0 +1,25 @@
+<?php
+include_once(__DIR__ .'../../classes/User.php');
+include_once(__DIR__ . '../../classes/Db.php');
+class showReaction{
+  
+/*
+$user = new User();
+session_start();
+$user->setEmail($_SESSION['user']);
+$connectedUserEmail= $_SESSION['user'];
+
+*/
+ 
+
+
+public static function showReactions(){
+     $conn = Db::getConnection();
+ $statement = $conn->prepare("select id, name, src from reaction");
+ $result = $statement->execute();
+
+ return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+  }
+
+}
