0a943f898692650179ff9638d16a6b8069acbc8e
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-05-03 22:29:47 +0200

    try to solve problem feature20

--- js/upvote.js
+++ js/upvote.js
@@ -3,11 +3,15 @@ console.log(upvotes); //geeft array terug met alle elementen die id upvote bevat
 
 upvotes.forEach(upvote => {
     upvote.addEventListener("click", function(){
+
+        upvotetry();
         let commentid = this.dataset.commentid;
         console.log(commentid);
         let number = parseInt(this.dataset.number);
         number = number + 1 ; 
         console.log(number);
+
+        
 
     
         let formData = new FormData();
