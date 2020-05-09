fceff45ec36da933b962264431b10e47c92d24c3
Esat <r0765162@student.thomasmore.be>
2020-05-09 19:48:37 +0200

    PHP&CSS:navbar restyled and responsive.

--- navbar.php
+++ navbar.php
@@ -18,26 +18,27 @@ if ($notification > 0) {
 
 ?>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top main-menu sortbuddy">
-    <div class="container sortBuddy">
-      <ul class="nav justify-content-end"> 
-       <a class="" id="profilImg" href="profile.php">
-        <div class="ProfilAvatar">
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top main-menu sortbuddy">
+   
+<ul class="tab-bar">
+  <li data-num="2" class="tab wave dark"><a href="feature22.php"><i class="fas fa-address-book"></i></a></li>
+  <li data-num="3" class="tab wave dark"><?php if (isset($showNotification)) : ?>
+  <div class="errorNotification"><?php echo $showNotification; ?></div><?php endif; ?>
+  <a href="messages.php"><i class="fas fa-comment"></i></a>
+  
+  </li>
+  <li data-num="2" class="tab wave dark avatar">
+    <a href="profile.php"><div class="ProfilAvatar">
        <img src="<?php if ($connectedUserPicture['picture'] === NULL) { echo"uploads/profilePic.png";} 
             else {echo "uploads/".$connectedUserPicture['picture'];} ?>" class="">
-        </div>
-        </a>
-      <a class="nav-link pt-4" href="feature22.php" id="contactIcon"><i class="fas fa-address-book"></i></a>
-      <a id="messageNotification" class="nav-link pt-4" href="messages.php"><i class="fas fa-comment"></i>
-       <?php if (isset($showNotification)) : ?>
-            <div class="errorNotification"><?php echo $showNotification; ?></div>
-          <?php endif; ?>
-      </a>
-       <a class="nav-link pt-4 comments " href="feature20.php"><i class="fas fa-sticky-note"></i></a>
-       <a class="nav-link pt-4 homeIcon" href="sortBuddy.php"><i class="fas fa-home"></i></a>
-       
-      </ul>
-    </div>
+        </div></a>
+    
+  </li>
+  <li data-num="4" class="tab wave dark"><a href="feature20.php"><i class="fas fa-sticky-note"></i></a></li>
+  <li data-num="5" class="tab wave dark"><a href="sortBuddy.php"><i class="fas fa-home"></i></a></li>
+  <div class="indicator"></div>
+</ul>
+  
   </nav>
-
   <script src="https://kit.fontawesome.com/6792ce1460.js" crossorigin="anonymous"></script>
