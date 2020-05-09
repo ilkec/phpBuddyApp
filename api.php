aa338319c7edd5d62ce07c924280c14f4197a7e2
Ilke Cauwenbergh <r0449955@student.thomasmore.be>
2020-05-09 22:39:48 +0200

    removed api.php file

--- api.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include_once(__DIR__ . '/classes/User.php');
-
-$user = new User();
- $api = 'SENDGRID_API_KEY=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';
-$user->setApi($api);
-?>
